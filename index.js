496b34c87dee08872a4244e0dedf44eceb958906
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-12-27 16:44:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let node = document.getElementById('main');
+if (node.parentNode) {
+  node.parentNode.removeChild(node)
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+newHeader.innerHTML = "Tanzima is the champion"
